274a5c0d7ff0717c0b0922846e4402426cac0747
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-08-12 08:54:59 +0530

    conversation completed

--- components/heading.tsx
+++ components/heading.tsx
@@ -19,13 +19,15 @@ export const Heading = ({
 }: HeadingProps) => {
   return (
     <>
-      <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+      <div className="px-4  border-white/10 lg:px-8 flex items-center gap-x-3 mb-8">
         <div className={cn("p-2 w-fit rounded-md", bgColor)}>
-          <Icon className={cn("w-10 h-10", iconColor)} />
+          <Icon className={cn("md:w-10 md:h-10 w-5 h-5", iconColor)} />
         </div>
         <div>
-          <h2 className="text-3xl text-white/80 font-bold">{title}</h2>
-          <p className="text-sm text-white/40 ">{description}</p>
+          <h2 className="md:text-3xl text-xl text-white/80 font-bold">
+            {title}
+          </h2>
+          <p className="md:text-sm text-xs  text-white/30 ">{description}</p>
         </div>
       </div>
     </>
